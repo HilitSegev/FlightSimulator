d039f7f1803afd86f9785001131a6cd8e4b81245
naamasharir <naamasharir3@gmail.com>
2021-04-13 01:04:51 +0300

    uploded dll in plugins

--- obj/Debug/netcoreapp3.0/Graphs.g.i.cs
+++ obj/Debug/netcoreapp3.0/Graphs.g.i.cs
@@ -94,7 +94,7 @@ namespace WPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPF;component/graphs.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPF;V1.0.0.0;component/graphs.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Graphs.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
